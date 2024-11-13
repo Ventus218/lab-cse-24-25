6c98839434c302a949038d609a69d11ad29131c4
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-11-13 11:53:59 +0100

    Update Config.scala

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -3,7 +3,7 @@ package utils
 object Config {
 
   // The local directory containing this repository
-  val projectDir :String = "C:\\Users\\alessandro.venturin6\\Desktop\\lab-cse-24-25"
+  val projectDir :String = "\\C:\\Users\\alessandro.venturin6\\Desktop\\lab-cse-24-25"
   // THe name of your bucket on AWS S3
   val s3bucketName :String = "unibo-bd2425-egallinucci"
   // The path to the credentials file for AWS (if you follow instructions, this should not be updated)
